e62216c6e702a6b6f215b1608ca552b3038a4468
Doge <doge@users.noreply.github.com>
2021-01-02 17:00:22 +0700

    class: Map
    Map  is inherited from SenseHat
    add bombs and explosions in Map
    add bombs, explosions handlers
    add parameter (Map) in Transform, adjust Transform and subclasses constructor
    remove bombs and explosions in Player

--- main.py
+++ main.py
@@ -9,12 +9,34 @@ def clamp(n, _min=0, _max=7):
 def valid_position_index(n):
     return 0 <= n <= 7
 
+class Map(SenseHat):
+    def __init__(self):
+        super().__init__()
+        self.bombs = []
+        self.explosions = []
+
+    def handle_bombs(self):
+        for bomb in self.bombs:
+            if bomb.should_explode():
+                bomb.explode()
+                self.bombs.remove(bomb)
+
+    def handle_explosions(self):
+        for explosion in self.explosions:
+            if explosion.should_end():
+                self.explosions.remove(explosion)
+            for bomb in self.bombs:
+                if explosion.is_collided_with(bomb):
+                    bomb.is_triggered = True
+    
+
 
 class Transform:
-    def __init__(self, x, y, color):
+    def __init__(self, map, x, y, color):
         self.x = x
         self.y = y
         self.color = color
+        self.map = map
 
     def get_position(self):
         return self.x, self.y
@@ -27,11 +49,9 @@ class Transform:
 
 
 class Player(Transform):
-    def __init__(self, _id=200, x=0, y=0, color = (0, 255, 0)):
-        super().__init__(x, y, color)
+    def __init__(self, map, _id=200, x=0, y=0, color = (0, 255, 0)):
+        super().__init__(map, x, y, color)
         self._id = _id
-        self.bombs = []
-        self.explosions = []
         self.is_dead = False
 
     def move(self, x, y):
@@ -39,31 +59,15 @@ class Player(Transform):
         self.y = clamp(self.y + y)
 
     def place_bomb(self):
-        for bomb in self.bombs:
+        for bomb in self.map.bombs:
             if self.is_collided_with(bomb):
                 return
-        self.bombs.append(Bomb(self.x, self.y, self))
+        self.map.bombs.append(Bomb(self.map, self.x, self.y, self))
         pass
 
-    def handle_bombs(self):
-        bombs = self.bombs[:]
-        for bomb in bombs:
-            if bomb.should_explode():
-                bomb.explode()
-
-    def handle_explosions(self):
-        explosions = self.explosions[:]
-        for explosion in explosions:
-            if explosion.should_end():
-                explosion.end()
-            for bomb in self.bombs:
-                if explosion.is_collided_with(bomb):
-                    bomb.is_triggered = True
-
-
 class Bomb(Transform):
-    def __init__(self, x, y , player, lifetime=2, length=2, color = (255, 0, 0)):
-        super().__init__(x, y, color)
+    def __init__(self, map, x, y , player, lifetime=2, length=2, color = (255, 0, 0)):
+        super().__init__(map, x, y, color)
         self.player = player
         self.is_triggered = False
         self.length = length
@@ -73,8 +77,7 @@ class Bomb(Transform):
         return self.is_triggered or time.time() > self.explode_time
 
     def explode(self):
-        player.bombs.remove(self)
-        player.explosions.extend(self.get_explosions())
+        self.map.explosions.extend(self.get_explosions())
 
     def get_explosions(self):
         tiles = []
@@ -87,28 +90,26 @@ class Bomb(Transform):
         for i in range(-length, length + 1):
             if valid_position_index(x + i):
                 tiles.append((x + i, y))
-        return [Explosion(tile[0], tile[1], self.player) for tile in set(tiles)]
+        return [Explosion(map, tile[0], tile[1], self.player) for tile in set(tiles)]
 
 
 class Explosion(Transform):
-    def __init__(self, x, y, player, lifetime=0.1, color = (255, 0, 255)):
-        super().__init__(x, y, color)
+    def __init__(self, map, x, y, player, lifetime=0.1, color = (255, 0, 255)):
+        super().__init__(map, x, y, color)
         self.player = player
         self.end_time = time.time() + lifetime
 
     def should_end(self):
         return time.time() > self.end_time
 
-    def end(self):
-        player.explosions.remove(self)
 
 
-sense = SenseHat()
+sense = Map()
 sense.clear()
 
 
-player = Player()
-virtual_player = Player(300, 7, 7, (255,255,0))
+player = Player(sense)
+virtual_player = Player(sense, 300, 7, 7, (255,255,0))
 players = [player, virtual_player]
 
 def move_up(event):
@@ -142,21 +143,21 @@ def render_players():
 
 
 def render_bombs():
-    for bomb in player.bombs:
+    for bomb in sense.bombs:
         bomb.render()
 
 
 def render_explosions():
-    for explosion in player.explosions:
+    for explosion in sense.explosions:
         explosion.render()
 
 
 def update():
     sense.clear()
     render_players()
-    player.handle_bombs()
+    sense.handle_bombs()
     render_bombs()
-    player.handle_explosions()
+    sense.handle_explosions()
     render_explosions()
 
 
