8203e83167bac3769e3c689829a4277dc1a86739
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-01-19 00:04:43 +0700

    'm'

--- fake_client.py
+++ fake_client.py
@@ -1,12 +1,17 @@
 import socketio
 
 sio = socketio.Client()
-URL = 'https://bomberrypi.herokuapp.com/'
+URL = 'https://bomberrypi.herokuapp.com/socket.io'
 
 
 @sio.event
 def connect():
     sio.emit('matchmaking')
+
+
+@sio.event()
+def start_game(pos):
+    print(pos)
 
 
 sio.connect(URL)
